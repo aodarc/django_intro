20e84284067c6e912391f5077386d95e5e7165b3
aodarc <odarchenko@ex.ua>
2020-05-03 17:17:19 +0300

    update db settings

--- src/config/settings/local.py
+++ src/config/settings/local.py
@@ -1,1 +1,12 @@
 from .base import *  #noqa
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_intro_local',
+        'USER': 'django_intro',
+        'PASSWORD': 'django_intro',
+        'HOST': 'postgres',
+        'PORT': 5432,
+    }
+}
