396bb9f6994cf2b23b7455e5233e5fa6a91402b8
aodarc <odarchenko@ex.ua>
2020-05-06 18:37:20 +0300

    update readme and env settings

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +26,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*eb-4x1!mgv52+07tzta8_^be*d+=65cn_)wnv1tu*sxh3gjyf'
+SECRET_KEY = env.str('SECRET_KEY', 'secret_key228')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -77,8 +83,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env.str('POSTGRES_DB', ''),
+        'USER': env.str('POSTGRES_USER', ''),
+        'PASSWORD': env.str('POSTGRES_PASSWORD', ''),
+        'HOST': env.str('DB_HOST', ''),
+        'PORT': env.int('DB_PORT', 5432),
     }
 }
 
@@ -119,8 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-MEDIA_ROOT = 'src/static'
-STATIC_ROOT = 'src/media'
+MEDIA_ROOT = env.str('MEDIA_ROOT', BASE_DIR)
+STATIC_ROOT = env.str('STATIC_ROOT', BASE_DIR)
 
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
--- src/config/settings/local.py
+++ src/config/settings/local.py
@@ -3,10 +3,10 @@ from .base import *  #noqa
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django_intro_local',
-        'USER': 'django_intro',
-        'PASSWORD': 'django_intro',
-        'HOST': 'postgres',
-        'PORT': 5432,
+        'NAME': env.str('POSTGRES_DB', 'django_intro_local'),
+        'USER': env.str('POSTGRES_USER', 'django_intro'),
+        'PASSWORD': env.str('POSTGRES_PASSWORD', 'django_intro'),
+        'HOST': env.str('DB_HOST', 'postgres'),
+        'PORT': env.int('DB_PORT', 5432),
     }
 }
